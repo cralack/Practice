4f8de95cfd547ebc46b10fac57a3f9960a511e70
K2ng <cralack@hotmail.com>
2019-12-18 19:14:11 +0800

    update

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,14 +1,36 @@
 #include<iostream>
 #include<cstring>
 #include<string>
+#include <cstdio>
+#include <sstream>
+#include <cstdlib>
 #include<memory>
 using namespace std;
 const char*  CodeAuthor = "Honduras Iron Egg";
 const int    N = 30;
 const double EPS = 1e-6;
 
-int main(void)
+class Sample {
+public:
+	int v;
+    Sample(int n){
+        return v;
+    }
+};
+void PrintAndDouble(Sample o)
 {
-    cout<<"Test1"<<endl;
-    return 0;
+	cout << o.v;
+	cout << endl;
+}
+int main()
+{
+	Sample a(5);
+	Sample b = a;
+	PrintAndDouble(b);
+	Sample c = 20;
+	PrintAndDouble(c);
+	Sample d;
+	d = a;
+	cout << d.v;
+	return 0;
 }
