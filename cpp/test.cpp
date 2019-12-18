94d7e6bac83ecca4c419a62ef82c72febeaab54f
K2ng <cralack@hotmail.com>
2019-12-18 15:38:07 +0800

    update

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
+#include<cstring>
+#include<string>
+#include<memory>
 using namespace std;
+const char*  CodeAuthor = "Honduras Iron Egg";
+const int    N = 30;
+const double EPS = 1e-6;
+
 int main(void)
 {
     cout<<"Test1"<<endl;
